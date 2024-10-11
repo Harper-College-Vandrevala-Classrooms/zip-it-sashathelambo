2d24460d366d0bda6dd30a1b487f4386fc21b5e7
sashathelambo <166897058+sashathelambo@users.noreply.github.com>
2024-10-11 12:24:24 -0500

    Update ZipperTest.java

--- src/test/java/com/csc/ZipperTest.java
+++ src/test/java/com/csc/ZipperTest.java
@@ -1,19 +1,26 @@
 package com.csc;
 
 import java.util.Arrays;
-import java.util.List; // Import List
+import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals; // Import Arrays
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
 public class ZipperTest {
 
-
   @Test
-  void testZip() {
+  void testZipIntegers() {
     List<Integer> list1 = Arrays.asList(1, 3, 5);
     List<Integer> list2 = Arrays.asList(2, 4, 6);
     List<Integer> expected = Arrays.asList(1, 2, 3, 4, 5, 6);
-    assertEquals(expected, Zipper.zip(list1, list2));
+    assertEquals(expected, Zipper.zipIntegers(list1, list2));
+  }
+
+  @Test
+  void testZipStrings() {
+    List<String> list1 = Arrays.asList("a", "c", "e");
+    List<String> list2 = Arrays.asList("b", "d", "f");
+    List<String> expected = Arrays.asList("a", "b", "c", "d", "e", "f");
+    assertEquals(expected, Zipper.zipStrings(list1, list2));
   }
 }
