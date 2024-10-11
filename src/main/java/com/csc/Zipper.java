3eb3e3db969b9ee0ff94c712ab98b1e1167c398a
sashathelambo <166897058+sashathelambo@users.noreply.github.com>
2024-10-11 12:17:22 -0500

    Update Zipper.java

--- src/main/java/com/csc/Zipper.java
+++ src/main/java/com/csc/Zipper.java
@@ -3,8 +3,21 @@
     import java.util.ArrayList;
     import java.util.List;
     public class Zipper {
-        public static <T> List<T> zip(List<T> list1, List<T> list2) {
-            List<T> merged = new ArrayList<>();
+        public static List<Integer> zipIntegers(List<Integer> list1, List<Integer> list2) {
+            List<Integer> merged = new ArrayList<>();
+            int minLength = Math.min(list1.size(), list2.size());
+            int i = 0;
+            for (; i < minLength; i++) {
+                merged.add(list1.get(i));
+                merged.add(list2.get(i));
+            }
+            merged.addAll(list1.subList(i, list1.size()));
+            merged.addAll(list2.subList(i, list2.size()));
+            return merged;
+        }
+
+        public static List<String> zipStrings(List<String> list1, List<String> list2) {
+            List<String> merged = new ArrayList<>();
             int minLength = Math.min(list1.size(), list2.size());
             int i = 0;
             for (; i < minLength; i++) {
@@ -27,7 +40,11 @@
             list2.add(4);
             list2.add(6);
 
-            List<Integer> result = zip(list1, list2);
+            List<Integer> result = zipIntegers(list1, list2);
             System.out.println(result);
+        }
+
+        public static Object zip(List<Integer> list1, List<Integer> list2) {
+            throw new UnsupportedOperationException("Unimplemented method 'zip'");
         }
     }
